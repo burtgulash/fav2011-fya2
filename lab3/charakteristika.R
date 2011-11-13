4b205ae7bccb2e9550cbc45d32c554fe15f93f5f
gusta <burtgulas@gmail.com>
2011-11-13 10:01:19 +0100

    lab3 fix

--- lab3/charakteristika.R
+++ lab3/charakteristika.R
@@ -13,20 +13,20 @@ max_indices <- data.frame(max_1=P[,1] == max(P[,1], na.rm=TRUE),
                           max_2=P[,2] == max(P[,2], na.rm=TRUE), 
                           max_3=P[,3] == max(P[,3], na.rm=TRUE))
 
-max_Us <- c(DATA[max_indices[[1]], "U_1"],
-            DATA[max_indices[[2]], "U_2"],
-            DATA[max_indices[[3]], "U_3"])
-
-max_Is <- c(DATA[max_indices[[1]], "I_1"],
-            DATA[max_indices[[2]], "I_2"],
-            DATA[max_indices[[3]], "I_3"])
-
-max_Us <- max_Us[!is.na(max_Us)]
-max_Is <- max_Is[!is.na(max_Is)]
-
-
-max_Us <- c(1.48, 1.54,1.57,1.69,1.72, 1.73)
-max_Is <- c(44.2, 44.4, 45.1, 58.5, 80.3, 95.0)
+# max_Us <- c(DATA[max_indices[[1]], "U_1"],
+#             DATA[max_indices[[2]], "U_2"],
+#             DATA[max_indices[[3]], "U_3"])
+# 
+# max_Is <- c(DATA[max_indices[[1]], "I_1"],
+#             DATA[max_indices[[2]], "I_2"],
+#             DATA[max_indices[[3]], "I_3"])
+# 
+# max_Us <- max_Us[!is.na(max_Us)]
+# max_Is <- max_Is[!is.na(max_Is)]
+# 
+# 
+# max_Us <- c(1.48, 1.54,1.57,1.69,1.72, 1.73)
+# max_Is <- c(44.2, 44.4, 45.1, 58.5, 80.3, 95.0)
 
 postscript(file="graf3.eps", encoding="CP1250")
 
@@ -40,12 +40,12 @@ points(x=DATA$U_3, y=DATA$I_3, col="green", pch=18, type="o")
 text(0.5, 48, labels=expression("E = 444 W/m"^2))
 
 
-lo <- loess(max_Is ~ max_Us)
-xx <- seq(min(max_Us), max(max_Us), (max(max_Us) - min(max_Us))/100)
-lines(xx, predict(lo, xx), col="gray", lty=2)
+# lo <- loess(max_Is ~ max_Us)
+# xx <- seq(min(max_Us), max(max_Us), (max(max_Us) - min(max_Us))/100)
+# lines(xx, predict(lo, xx), col="gray", lty=2)
+# text(1.81, 90, labels=expression("P"[max]))
 
 
-text(1.81, 90, labels=expression("P"[max]))
 
 title(main="Voltampérmetrová charakteristika solární baterie")
 title(xlab="I [mA]", ylab="U [V]")
--- lab3/intenzita.R
+++ lab3/intenzita.R
@@ -7,20 +7,37 @@ E          <- U / citlivost
 # print Es'
 cat(E, sep="\n")
 
+postscript(file="graf0.eps", encoding="CP1250")
+plot(x=data$a, y=E, col="red", lty=1, pch=15, xlab="", ylab="")
+abline(lm(E ~ data$a), col="gray", lty=2, lwd=2)
+
+title(main=expression("Závislost intenzity ozáření na vzdálenosti"), 
+                                                      cex.main=1.5)
+title(xlab="a [cm]", cex.lab=1.5)
+title(ylab=expression("E [Wm"^-2~"]"), cex.lab=1) 
+
+dev.off()
+
 
 # plot 1
 postscript(file="graf1.eps", encoding="CP1250")
 plot(x=E, y=data[["U_0"]], col="blue", lty=1, pch=15, xlab="", ylab="")
+abline(lm(data$U_0 ~ E), col="gray", lty=2, lwd=2)
+
 title(main=expression("Závislost U"[0]~" na E"), cex.main=1.5)
-title(xlab=expression("E [Wm"^-2~"]", cex.lab=2), cex.lab=1.5) 
-title(ylab=expression("U"[0]), cex.lab=2)
+title(xlab=expression("E [Wm"^-2~"]"), cex.lab=1) 
+title(ylab=expression("U"[0]~"[V]"), cex.lab=1)
 dev.off()
 
+
+# plot 2
 postscript(file="graf2.eps", encoding="CP1250")
 plot(x=E, y=data[["I_f"]], xlab="", ylab="", col="green", lty=1, pch=15)
+abline(lm(data$I_f ~ E), col="gray", lty=2, lwd=2)
+
 title(main=expression("Závislost I"[f]~" na E"), cex.main=1.5)
-title(xlab=expression("E [Wm"^-2~"]", cex.lab=2), cex.lab=1.5) 
-title(ylab=expression("I"[f]), cex.lab=2)
+title(xlab=expression("E [Wm"^-2~"]"), cex.lab=1) 
+title(ylab=expression("I"[f]~"[mA]"), cex.lab=1)
 dev.off()
 
 print(lm(data[["I_f"]] ~ E - 1))
