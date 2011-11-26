2caf20838e49d58a90713263b26a7340125eaf4a
gusta <burtgulas@gmail.com>
2011-11-26 13:19:58 +0100

    lab4 pocty ne vsechny jsete

--- lab4/charakteristika.R
+++ lab4/charakteristika.R
@@ -5,8 +5,16 @@ n <- c(0.1, 158, 167, 152, 162, 165, 149, 165, 178, 161, 198, 229)
 cetnost <- n * 60 / 100
 conf_interval <- sqrt(n) * 60 / 100
 
+plato <- lm(n[2:6] ~ voltaz[2:6]) 
+
 postscript(file="charakteristika.eps", encoding="CP1250")
 plotCI(voltaz, cetnost, uiw=conf_interval, gap=0, xlim=c(200, 780)
       , xlab="napětí [V]", ylab="četnost [imp/min]"
       , main="Charakteristika GM počítače")
 dev.off()
+
+print(plato)
+
+x_coef <- 0.0225
+print ("Sklon plata:")
+print (x_coef * 100 / n[2] * 100)
---
+++ lab4/doba.R
@@ -0,0 +1,15 @@
+R0 = 302 / 200
+R01 = 9288 / 200
+R02 = 8865 / 200
+R012 = 17670 / 200
+
+t = (R01 + R02 - R012 - R0) / (2 * (R01 - R0) * (R02 - R0))
+tR = t * (1 + t/2 * (R012 - R0))
+
+D012 = R012 / (1 - R012 * tR)
+
+print ("Rozlišovací doba:")
+print (tR)
+
+print ("Ztráta impulzů pro oba zářiče:")
+print (D012)
