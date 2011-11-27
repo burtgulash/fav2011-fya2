22c9730601c8313bad4f7ee24b32fddf2ceec8dc
gusta <burtgulas@gmail.com>
2011-11-27 15:44:27 +0100

    jeste plato

--- lab4/doba.R
+++ lab4/doba.R
@@ -6,10 +6,13 @@ R012 = 17670 / 200
 t = (R01 + R02 - R012 - R0) / (2 * (R01 - R0) * (R02 - R0))
 tR = t * (1 + t/2 * (R012 - R0))
 
-D012 = R012 / (1 - R012 * tR)
+D012 = R012 / (1 - R012 * tR) - R012
 
 print ("Rozlišovací doba:")
 print (tR)
 
 print ("Ztráta impulzů pro oba zářiče:")
 print (D012)
+
+print ("Relativní ztráta:")
+print (D012 / R012)
